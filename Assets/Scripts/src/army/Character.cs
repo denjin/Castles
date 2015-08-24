dab80501c28122c83625a6b4916157905d084a41
denjin <luffmcgruff@hotmail.com>
2015-08-24 20:38:50 +0100

    Character storage groundwork

---
+++ Assets/Scripts/src/army/Character.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using Gear;
+
+namespace Armies {
+	public class Character : Soldier {
+		private int diplomacy;
+		private int martial;
+		private int intrigue;
+	}
+}
